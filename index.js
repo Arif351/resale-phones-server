b9140a1393300fe9f3848bce4c26c9583486301d
Arif351 <ariful8836@gmail.com>
2022-12-02 02:03:03 +0600

    blog

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -16,6 +17,8 @@ async function run() {
     try {
         const allBrandsCollection = client.db('cellRoom').collection('allPhonesAndCategory');
         const bookingsCollection = client.db('cellRoom').collection('bookingPhones');
+        const usersCollection = client.db('cellRoom').collection('usersCollection');
+        const blogCollection = client.db('cellRoom').collection('blog');
 
         app.get('/allPhones', async (req, res) => {
             const query = {};
@@ -30,6 +33,12 @@ async function run() {
             res.send(allCategories)
         });
 
+        app.get('/blog', async (req, res) => {
+            const query = {};
+            const blogsAnswer = await blogCollection.find(query).toArray();
+            res.send(blogsAnswer)
+        })
+
         app.post('/bookings', async (req, res) => {
             const bookings = req.body
             console.log(bookings);
@@ -43,6 +52,13 @@ async function run() {
             const query = { email: email }
             const bookings = await bookingsCollection.find(query).toArray();
             res.send(bookings);
+        });
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
+            console.log(result);
+            res.send(result)
         })
 
     }
